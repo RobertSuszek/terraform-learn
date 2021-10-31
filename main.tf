ede6ca18abd04f7b85b968efb539252224676520
Robert Suszek <robert.suszek96@gmail.com>
2021-10-31 02:05:11 +0100

    Creating 2 EC2s with FGs

---
+++ main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "rsuszek-learn"
+  region  = "eu-central-1"
+}
+
+resource "aws_instance" "fg_normal" {
+  ami           = "ami-090da6c908c1127f6"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "FGNormalImage"
+  }
+}
+
+resource "aws_instance" "fg_ondemand" {
+  ami           = "ami-07e1b42208e73e245"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "FGOnDemandImage"
+  }
+}
