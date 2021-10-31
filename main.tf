d291199cb1bee6caa423fa213bf05f513592a39f
Robert Suszek <robert.suszek96@gmail.com>
2021-10-31 10:30:09 +0100

    Referencing template file, fixing naming

--- main.tf
+++ main.tf
@@ -13,16 +13,17 @@ provider "aws" {
   region  = var.region
 }
 
-resource "aws_instance" "FortigateTest" {
+resource "aws_instance" "fortigate_test" {
   ami           = var.fortigate_AMI["7.0.2"]
   instance_type = var.size
   key_name      = var.key_name
+  user_data     = data.template_file.fortigate_initial_config.rendered
 
   tags = {
-    Name = "FGOnDemandImage"
+    Name = "Fortigate Test"
   }
 }
 
-data "template_file" "FortiGate" {
-  template = file("${var.bootstrap-fgtvm}")
+data "template_file" "fortigate_initial_config" {
+  template = file("${var.fortigate_initial_config_path}")
 }
